81cb41427525054d5c9f149a0f753fa04b136e94
Alex-V2 <Alexander-Vogt1@gmx.de>
2013-05-24 20:14:23 +0200

    version_changes

--- drivers/net/wireless/compat-wireless_R5.SP2.03/include/linux/compat_autoconf.h
+++ drivers/net/wireless/compat-wireless_R5.SP2.03/include/linux/compat_autoconf.h
@@ -2,7 +2,7 @@
 #define COMPAT_AUTOCONF_INCLUDED
 /*
  * Automatically generated C config: don't edit
- * Sa 11. Mai 22:19:17 CEST 2013 
+ * Fr 24. Mai 20:08:52 CEST 2013 
  * compat-wireless-2.6: ol_R5.00.14
  * linux-2.6: ol_R5.00.21-23-g618c7e5
  */
